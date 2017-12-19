c54f4754dfcd4a63e38a4ebf822ce4fdda75eeb4
mariastlouis <msantray@yahoo.com>
2017-12-19 16:51:23 -0700

    initial file structure

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './components/App/App';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import registerServiceWorker from './registerServiceWorker';
+import rootReducer from './reducers';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(rootReducer, devTools, applyMiddleware(thunk, logger));
+
+// const store = createStore(devTools, applyMiddleware(thunk, logger));
+
+const router = (
+  <Provider store = {store}>
+    <BrowserRouter>
+      <App store = {store} />
+    </BrowserRouter>
+  </Provider>
+);
+
+ReactDOM.render(router, document.getElementById('root'));
+registerServiceWorker();
---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+// import makeMoviesReducer from './makeMoviesReducer/makeMoviesReducer';
+// import userReducers from './userReducers/userReducers';
+
+const rootReducer = combineReducers({
+  // movies: makeMoviesReducer,
+  // user: userReducers
+});
+
+export default rootReducer;
