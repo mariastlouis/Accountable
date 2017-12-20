ab9397d1aadde9ed284d2df4355ac63512733cc5
mariastlouis <msantray@yahoo.com>
2017-12-19 22:54:19 -0700

    lawmaker images added

--- src/containers/Contact/Contact.js
+++ src/containers/Contact/Contact.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Contact = ({firstName, lastName}) => {
+const Contact = ({firstName, lastName, party, image, website, phone, occupation, district, chamber}) => {
   return (
     <div className = "Contact">
-    <h1> {firstName} {lastName} </h1>
+      <h1> {firstName} {lastName} </h1>
+      <img className = "mugshot" src = {image} alt = {`${firstName} ${lastName}`} />
     </div>
   );
 };
--- src/containers/Details/DetailsPage.js
+++ src/containers/Details/DetailsPage.js
@@ -3,12 +3,18 @@ import Contact from '../Contact/Contact';
 import PropTypes from 'prop-types';
 
 const DetailsPage = ({data}) => {
-  // let dataKeys = Object.keys(data);
   const mappedContact = Object.keys(data).map((dataPoint) =>{
     return (
       <Contact 
         firstName = {data[dataPoint].firstName}
-        lastName = {data[dataPoint].lastName}/>
+        lastName = {data[dataPoint].lastName}
+        party = {data[dataPoint].party}
+        image = {data[dataPoint].image}
+        website = {data[dataPoint].website}
+        phone = {data[dataPoint].phone}
+        occuption = {data[dataPoint].occuption}
+        district = {data[dataPoint].district}
+        chamber = {data[dataPoint].chamber} />
     );
   });
 
