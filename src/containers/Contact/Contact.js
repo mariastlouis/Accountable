627bb4a75ccf0f0a14cafc0471fad9bacecc12dd
mariastlouis <msantray@yahoo.com>
2017-12-20 08:54:34 -0700

    Changed structure of data

--- src/containers/Contact/Contact.js
+++ src/containers/Contact/Contact.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Contact = ({firstName, lastName, party, image, website, phone, occupation, district, chamber}) => {
+const Contact = ({id, firstName, lastName, party, image, website, phone, occupation, district, chamber}) => {
   return (
     <div className = "Contact">
       <h2> {firstName} {lastName} </h2>
@@ -9,6 +9,7 @@ const Contact = ({firstName, lastName, party, image, website, phone, occupation,
         <img className = "mugshot" src = {image} alt = {`${firstName} ${lastName}`} />
         <p> {party} </p>
         <p> District: {district} </p>
+        <p> ID: {id} </p>
       </div>
       <div className = "profile">
         <ul>
--- src/containers/Details/DetailsPage.js
+++ src/containers/Details/DetailsPage.js
@@ -3,18 +3,20 @@ import Contact from '../Contact/Contact';
 import PropTypes from 'prop-types';
 
 const DetailsPage = ({data}) => {
+  console.log(data[0].contact)
   const mappedContact = Object.keys(data).map((dataPoint) =>{
     return (
       <Contact 
-        firstName = {data[dataPoint].firstName}
-        lastName = {data[dataPoint].lastName}
-        party = {data[dataPoint].party}
-        image = {data[dataPoint].image}
-        website = {data[dataPoint].website}
-        phone = {data[dataPoint].phone}
-        occupation = {data[dataPoint].occupation}
-        district = {data[dataPoint].district}
-        chamber = {data[dataPoint].chamber} />
+        id = {data[dataPoint].id}
+        firstName = {data[dataPoint].contact.firstName}
+        lastName = {data[dataPoint].contact.lastName}
+        party = {data[dataPoint].contact.party}
+        image = {data[dataPoint].contact.image}
+        website = {data[dataPoint].contact.website}
+        phone = {data[dataPoint].contact.phone}
+        occupation = {data[dataPoint].contact.occupation}
+        district = {data[dataPoint].contact.district}
+        chamber = {data[dataPoint].contact.chamber} />
     );
   });
 
