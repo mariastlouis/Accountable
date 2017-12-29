c887b100cb1f8704319ecfeb41273f99a9ca4d5f
mariastlouis <msantray@yahoo.com>
2017-12-28 22:15:36 -0700

    photos are circular

--- src/containers/Contact/Contact.js
+++ src/containers/Contact/Contact.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Contact.css'
+
 
 const Contact = ({id, firstName, lastName, party, image, website, phone, occupation, district, chamber}) => {
   return (
     <div className = "Contact">
-      <h2> {firstName} {lastName} </h2>
-      <div className = "intro">
-        <img className = "mugshot" src = {image} alt = {`${firstName} ${lastName}`} />
-        <p> {party} </p>
-        <p> District: {district} </p>
-        <p> ID: {id} </p>
+      <div className = "left-side-info">
+        <div className = "lawmaker-name">
+          <h2> {firstName} {lastName} </h2>
+        </div>
+        <div className = "intro">
+          <div className = "mugshot-image">
+            <img className = "mugshot" src = {image} alt = {`${firstName} ${lastName}`} />
+          </div>
+          <p> {party} </p>
+          <p> District: {district} </p>
+          <p> ID: {id} </p>
+        </div>
       </div>
-      <div className = "profile">
+      <div className = "right-side-info">
         <ul>
           <li> <a href = {website}>Visit website </a> </li>
           <li> Phone: {phone} </li>
